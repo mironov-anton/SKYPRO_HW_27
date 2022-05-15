ed3b021538980792476760c389c66e95b4763cef
mironov-anton <miron.khuzd@gmail.com>
2022-05-15 23:21:30 +0300

    fixed structure, made an 'ads' app

--- ads/views.py
+++ ads/views.py
@@ -1,12 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+import json
+
 from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import DetailView
+
+from ads.models import Ad, Category
 
 
 def index(request):
     return HttpResponse(b'200, {"status": "ok"}')
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class AdView(View):
     def get(self, request):
         ads = Ad.objects.all()
@@ -20,12 +30,35 @@ class AdView(View):
             })
         return JsonResponse(response, safe=False)
 
+    def post(self, request):
+        ad_data = json.loads(request.body)
+        ad = Ad.objects.create(
+            name=ad_data["name"],
+            author=ad_data["author"],
+            price=ad_data["price"],
+            description=ad_data["description"],
+            address=ad_data["address"],
+            is_published=ad_data["is_published"]
+        )
+        return JsonResponse({
+            "id": ad.id,
+            "name": ad.name,
+            "author": ad.author,
+            "price": ad.price,
+            "description": ad.description,
+            "address": ad.address,
+            "is_published": ad.is_published
+        })
+
 
 class AdDetailView(DetailView):
     model = Ad
 
     def get(self, request, *args, **kwargs):
-        ad = self.get_object()
+        try:
+            ad = self.get_object()
+        except Ad.DoesNotExist:
+            return JsonResponse({"error": "Not found"}, status=404)
 
         return JsonResponse({
             "id": ad.id,
@@ -38,6 +71,7 @@ class AdDetailView(DetailView):
         })
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class CategoryView(View):
     def get(self, request):
         categories = Category.objects.all()
@@ -49,12 +83,25 @@ class CategoryView(View):
             })
         return JsonResponse(response, safe=False)
 
+    def post(self, request):
+        category_data = json.loads(request.body)
+        category = Category.objects.create(
+            name=category_data["name"]
+        )
+        return JsonResponse({
+            "id": category.id,
+            "name": category.name
+        })
+
 
 class CategoryDetailView(DetailView):
     model = Category
 
     def get(self, request, *args, **kwargs):
-        category = self.get_object()
+        try:
+            category = self.get_object()
+        except Category.DoesNotExist:
+            return JsonResponse({"error": "Not found"}, status=404)
 
         return JsonResponse({
             "id": category.id,
--- bvito/wsgi.py
+++ bvito/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ads project.
+WSGI config for bvito project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ads.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bvito.settings')
 
 application = get_wsgi_application()
